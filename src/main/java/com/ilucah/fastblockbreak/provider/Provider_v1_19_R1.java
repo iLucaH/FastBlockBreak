11a7ba695f715b3fdc35ad6d2a0c79e074374682
iLucaH <lucahautrive@gmail.com>
2022-06-11 08:39:58 +1000

    1.19 Update to fix incompatibility message.

---
+++ src/main/java/com/ilucah/fastblockbreak/provider/Provider_v1_19_R1.java
@@ -0,0 +1,16 @@
+package com.ilucah.fastblockbreak.provider;
+
+import org.bukkit.Location;
+import org.bukkit.Material;
+
+public class Provider_v1_19_R1 implements NMSProvider {
+
+    /*
+    As of v1_17_R1 #setType() is already blazing fast
+     */
+    @Override
+    public void setBlockFast(Location location, Material material, boolean applyPhysics) {
+        location.getBlock().setType(material, applyPhysics);
+    }
+
+}
